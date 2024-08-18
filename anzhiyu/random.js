48a68a377ebfad5c86e38cd0bc2d8e98a3f35735
kongziliuciqing <kongziliuciqing@outlook.com>
2024-08-18 13:39:33 +0000

    Site updated: 2024-08-18 13:39:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["undefined这是一篇新的博文2/","undefined这是一篇新的博文/","undefinedhello-world/"];function toRandomPost(){
+var posts=["undefined这是一篇新的博文2/","undefined这是一篇新的博文/","undefinedTextr-Go-最好用的在线接打电话神奇-免费接打电话/","undefinedhello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
